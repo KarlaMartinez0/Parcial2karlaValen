9acf461d20c3900847371ed50c006a42ab648229
amart <amart@10.0.15.52>
2025-04-08 21:03:27 -0500

    Codigo de simulacion de batallas basic

--- src/parcial2karlayvalen/Parcial2karlayValen.java
+++ src/parcial2karlayvalen/Parcial2karlayValen.java
@@ -5,7 +5,20 @@ package parcial2karlayvalen;
 public class Parcial2karlayValen {
 
  static void main(String[] args) {
-        // TODO code application logic here
+        Criatura dragon = new Dragon("Dragón", 100, 20);
+        Criatura mago = new Mago("Mago", 80, 15);
+        Criatura guerrero = new Guerrero("Guerrero", 90, 18);
+
+ 
+        dragon.atacar(mago);
+        System.out.println(mago.nombre + " salud: " + mago.salud);
+        
+        mago.atacar(guerrero);
+        System.out.println(guerrero.nombre + " salud: " + guerrero.salud);
+        
+        guerrero.atacar(dragon);
+        System.out.println(dragon.nombre + " salud: " + dragon.salud);
+    
     }
     
 }
